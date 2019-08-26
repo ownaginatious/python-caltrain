488a634f6679b705d3a3f6cce92a1edd99087d08
ownaginatious <github@dillon.io>
2019-08-26 17:00:14 -0500

    Drop versioneer (#4)

--- python_caltrain/__init__.py
+++ python_caltrain/__init__.py
@@ -2,6 +2,4 @@ from .caltrain import (
     Caltrain, Train, Trip, TransitType, Station, Stop, Direction, UnknownStationError,
     UnexpectedGTFSLayoutError)
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from .__version__ import __version__
