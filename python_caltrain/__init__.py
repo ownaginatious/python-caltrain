0b3874a7b277f7aecf6cff36a672ecbd603eedb0
Dillon Dixon <dillondixon@gmail.com>
2019-08-26 15:21:15 -0700

    Use black for code formatting

--- python_caltrain/__init__.py
+++ python_caltrain/__init__.py
@@ -1,5 +1,13 @@
 from .caltrain import (
-    Caltrain, Train, Trip, TransitType, Station, Stop, Direction, UnknownStationError,
-    UnexpectedGTFSLayoutError)
+    Caltrain,
+    Train,
+    Trip,
+    TransitType,
+    Station,
+    Stop,
+    Direction,
+    UnknownStationError,
+    UnexpectedGTFSLayoutError,
+)
 
 from .__version__ import __version__
